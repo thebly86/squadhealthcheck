006e8979cc26abbda1e5f3c1f4e3a320c6d1c47b
Sean Smith (UK Leeds) <sean.smith3@baesystems.com>
2018-06-27 11:26:17 +0100

    Added a modal for deleting a team

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -8,11 +8,23 @@
         <i class="icon icon--edit fa fa-edit"></i>
       </a>
       <a
-        @click="deleteTeam"
+        @click="showDeleteModal = true"
         class="btn-action">
         <i class="icon icon--delete fa fa-trash"></i>
       </a>
     </td>
+    <Modal
+      v-if="showDeleteModal"
+      title="Delete Team"
+      :actions="deleteActions"
+      @close="showDeleteModal = false">
+      <div slot="body">
+        <p class="text-center">
+          Are you sure you want to delete this team? <br/><br/>
+          <b>The team will be deleted permanently.</b>
+        </p>
+      </div>
+    </Modal>
   </tr>
 </template>
 
@@ -20,6 +32,7 @@
 <script>
   import store from '../store/index.js'
   import FirebaseService from '../utils/firebase/firebase-service.js';
+  import Modal from './Modal';
 
   export default {
     name: 'Team',
@@ -30,15 +43,43 @@
         required: true
       }
     },
+
+    components: {
+      Modal
+    },
+
+    computed: {
+      deleteActions: function() {
+        return [
+          {
+            name: 'Delete',
+            class: 'btn-primary btn--danger',
+            action: this.deleteTeam
+          }, 
+          {
+            name: 'Cancel',
+            class: 'btn-secondary',
+            action: () => this.showDeleteModal = false
+          }
+        ]
+      }
+    },
+
+    data: () => ({
+      showDeleteModal: false
+    }),
+
     methods: {
       deleteTeam() {
         const projectId = this.$route.params.id;
         
         FirebaseService.removeTeam(projectId, this.team.id);
+
         store.commit('removeTeamFromProject', {
-          projectId: projectId,
+          projectId,
           teamId: this.team.id
         });
+        this.showDeleteModal = false
       }
     }
   }
